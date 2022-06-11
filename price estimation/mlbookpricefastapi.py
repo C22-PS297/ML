b86289ba162d9ec60a2fc398f5339d07a636e7fc
Muhammad Ridzky Hanura <89993213+ridzkyhanura@users.noreply.github.com>
2022-06-11 20:57:44 +0700

    add fastapi file

---
+++ price estimation/mlbookpricefastapi.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI 
+import uvicorn
+import tensorflow as tf
+from tensorflow import keras
+
+app = FastAPI(debug=True)
+
+@app.get('/')
+def home():
+    return{'text': "Book Price Prediction"}
+
+@app.get('/ predict')
+def predict(Amount: float):
+    model = tf.keras.models.load_model('D:\price estimation\estpricekilosbooks.h5')
+    makeprediction = model.predict([Amount])
+    output = makeprediction[0]
+    return{"Selling price of {} kg books = {}".format(Amount, output)}
+
+if __name__ == '__main__':
+    uvicorn.run(app)
