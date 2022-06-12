7240353b31803bfdacc1a058fc0eedfdd0bf7793
Muhammad Ridzky Hanura <89993213+ridzkyhanura@users.noreply.github.com>
2022-06-12 14:31:00 +0700

    add docker for ML price estimation model

---
+++ estpricekilosbooks/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI 
+import uvicorn
+import tensorflow as tf
+from tensorflow import keras
+
+app = FastAPI(debug=True)
+model = tf.keras.models.load_model("app/model/estpricekilosbooks.h5")
+
+@app.get('/')
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.post('/ predict')
+async def predict(amount: float):
+    prediction = model.predict([amount])
+    output = prediction[0]
+    return{"price": output}
+
+if __name__ == '__main__':
+    uvicorn.run("app", port=8000, reload=True)
